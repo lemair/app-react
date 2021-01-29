f6f2a5880607f7aed470821bb584178b050d9025
lemair <blacklemair@gmail.com>
2021-01-29 21:06:30 +0200

    Initial commit

---
+++ React/my-app/src/components/pages/SignUp/SignUp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HeroSection from '../HeroSection';
+import Pricing from '../Pricing';
+import {homeObjThree} from './Data';
+
+
+function SignUp() {
+    return(
+        <>
+          <Pricing/>
+          <HeroSection {...homeObjThree}/> 
+        
+        </>
+    );
+}
+
+export default SignUp;
